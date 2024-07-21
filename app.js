aa9b5e35cabda4650fdeb8bc22f0c1eeaa560e18
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-07-21 18:08:52 +0500

    add image as cond

--- app.js
+++ app.js
@@ -70,8 +70,8 @@ const getweather = (city) => {
                 document.body.style.backgroundImage = "url('Images/rainybg.jpg')";
             } else if (weatherMain.includes('snowy')) {
                 document.body.style.backgroundImage = "url('Images/snowbg.jpg')";
-            // } else if (weatherMain.includes('haze') || weatherMain.includes('smoke')) {
-                document.body.style.backgroundImage = "url('Images/hazebg.webp')";
+            } else if (weatherMain.includes('haze') || weatherMain.includes('smoke')) {
+                // document.body.style.backgroundImage = "url('Images/hazebg.webp')";
             } else if (weatherMain.includes('thunderstorm')) {
                 document.body.style.backgroundImage = "url('Images/thunderstormbg.jpeg')";
             } else {
