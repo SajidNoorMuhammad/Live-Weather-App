b6773dd68cc30014263e10481a64e448c1a2daba
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-07-21 17:49:39 +0500

    fetch the api url.

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ document.getElementById("weatherForm").addEventListener('submit', (event) => {
 const getweather = (city) => {
     const APIkey = "4f62b4511aae25559f2ae77ae94ab77a";
     const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`
-    // const cityName = document.getElementById("city")
-    fetch(apiurl)
+    const cityName = document.getElementById("city")
+    // fetch(apiurl)
         .then(response => response.json())
         .then(data => {
             const weatherResult = document.getElementById('weatherResult');
